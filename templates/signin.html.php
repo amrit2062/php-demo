b1e6411d12153a554e2d22c81ff5c9b32d4ca072
Amrit Paudel <amritpaudel2062@gmail.com>
2025-08-28 16:34:36 +0545

    added loginpage

--- templates/signin.html.php
+++ templates/signin.html.php
@@ -1,46 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title></title>
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    
-        <form class="signin-form" action="/login" method="post">
-            <h1>Sign In</h1>
+<?php require_once TEMPLATE_PATH . 'common/header.html.php' ?>
 
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" value="<?= $request->post->get('username', '') ?>" id="username" name="username">
-
-                <?php if ($errors->has('username')): ?>
-                    <div class="error-message">
-                        <?php echo $errors->get('username'); ?>
+<section class="page-section" id="contact">
+            <div class="container">
+                <!-- Contact Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Login</h2>
+                <!-- Icon Divider-->
+                <!-- Contact Section Form-->
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 col-xl-7">
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- * * SB Forms Contact Form * *-->
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- This form is pre-integrated with SB Forms.-->
+                        <!-- To make this form functional, sign up at-->
+                        <!-- https://startbootstrap.com/solution/contact-forms-->
+                        <!-- to get an API token!-->
+                        <form  method="post" action="/login">
+                            <!-- Name input-->
+                            
+                            <!-- Email address input-->
+                            <div class="form-floating mb-3">
+                                <input class="form-control" value="<?= $request->post->get('username', '') ?>"  id="email" type="text" placeholder="name@example.com" name="username" />
+                                <label for="email">Email address</label>
+                                
+                                <?php if ($errors->has('username')): ?>
+                                    <div class="error-message">
+                                        <?php echo $errors->get('username'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                            <!-- Password address input-->
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="password" type="password" placeholder="Enter your password" name="password" />
+                                <label for="password">Password</label>
+                                
+                                <?php if ($errors->has('password')): ?>
+                                    <div class="error-message">
+                                        <?php echo $errors->get('password'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                            <!-- Submit Button-->
+                            <button class="btn btn-primary btn-xl" id="submitButton" type="submit">Login</button>
+                        </form>
                     </div>
-                <?php endif; ?>
+                </div>
             </div>
+        </section>
 
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password">
-
-
-                <?php if ($errors->has('password')): ?>
-                    <div class="error-message">
-                        <?php echo $errors->get('password'); ?>
-                    </div>
-                <?php endif; ?>
-            </div>
-
-            <div class="form-group">
-                <button type="submit">Sign In</button>
-            </div>
-
-
-        </form>
-
-
-    </body>
-</html>
+<?php require_once TEMPLATE_PATH . 'common/footer.html.php' ?>
